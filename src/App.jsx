64df81666d86cbcea6977ffc847e4827e778e5cd
Lutow <118168487+Lutow@users.noreply.github.com>
2025-10-03 14:34:08 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home/Home.jsx";
 import './index.css'
+import Register from "./Login+Register/Register.jsx";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<Register />} />
         </Routes>
     );
 }
